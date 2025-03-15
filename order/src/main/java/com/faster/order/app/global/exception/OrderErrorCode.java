96016eb8a68f45d00d9b6fb51a6550a8a41f4803
HanaHww2 <hanahww2@gmail.com>
2025-03-15 23:22:45 +0900

    [#21][order] style: OrderErrorCode 줄바꿈

--- order/src/main/java/com/faster/order/app/global/exception/OrderErrorCode.java
+++ order/src/main/java/com/faster/order/app/global/exception/OrderErrorCode.java
@@ -9,15 +9,18 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum OrderErrorCode implements ErrorCode {
 
-  NOT_ENOUGH_TOTAL_PRICE(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+  NOT_ENOUGH_TOTAL_PRICE(
+      HttpStatus.UNPROCESSABLE_ENTITY.value(),
       "결제 금액이 최소 결제 금액 3000원보다 적습니다.",
       HttpStatus.UNPROCESSABLE_ENTITY),
-  INVALID_ORDER_ID(HttpStatus.NOT_FOUND.value(),
+  INVALID_ORDER_ID(
+      HttpStatus.NOT_FOUND.value(),
       "존재하지 않는 주문 아이디입니다.",
       HttpStatus.NOT_FOUND),
-  UNABLE_REMOVE(HttpStatus.UNPROCESSABLE_ENTITY.value(),
+  UNABLE_REMOVE(
+      HttpStatus.UNPROCESSABLE_ENTITY.value(),
       "주문은 취소, 완료 상태만 삭제 가능합니다.",
-      HttpStatus.UNPROCESSABLE_ENTITY),;
+      HttpStatus.UNPROCESSABLE_ENTITY);
 
   private final int code;
   private final String message;
