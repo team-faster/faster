4592fe759dae13f4b327f3ea5ad81865a5860380
HanaHww2 <hanahww2@gmail.com>
2025-03-15 23:21:18 +0900

    [#21][order] refactor: CurrentUserInfo 추가

--- order/src/main/java/com/faster/order/app/order/application/usecase/OrderServiceImpl.java
+++ order/src/main/java/com/faster/order/app/order/application/usecase/OrderServiceImpl.java
@@ -1,13 +1,12 @@
 package com.faster.order.app.order.application.usecase;
 
 import com.common.exception.CustomException;
+import com.common.resolver.dto.CurrentUserInfoDto;
 import com.faster.order.app.global.exception.OrderErrorCode;
 import com.faster.order.app.order.application.dto.response.OrderDetailApplicationResponseDto;
 import com.faster.order.app.order.domain.entity.Order;
-import com.faster.order.app.order.domain.enums.OrderStatus;
 import com.faster.order.app.order.domain.repository.OrderRepository;
 import java.time.LocalDateTime;
-import java.util.Set;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,7 +20,7 @@ public class OrderServiceImpl implements OrderService {
   private final OrderRepository orderRepository;
 
   @Override
-  public OrderDetailApplicationResponseDto getOrderById(UUID orderId) {
+  public OrderDetailApplicationResponseDto getOrderById(CurrentUserInfoDto userInfo, UUID orderId) {
 
     // todo. 유저정보에 따라 주문 정보 접근 권한 체크
     // 마스터 - 모든 주문 조회 가능, 업체 담당자 - 해당 업체 주문 조회 가능
@@ -33,7 +32,7 @@ public class OrderServiceImpl implements OrderService {
   }
 
   @Override
-  public void deleteOrderById(UUID orderId) {
+  public void deleteOrderById(CurrentUserInfoDto userInfo, UUID orderId) {
     // todo. 유저정보에 따라 주문 정보 접근 권한 체크
     // 삭제는 완료 혹은 취소 상태인 주문에만 가능
     // 마스터 - 모든 업체 주문 삭제 가능, 업체 담당자 - 해당 업체 주문 삭제 가능
@@ -44,6 +43,8 @@ public class OrderServiceImpl implements OrderService {
     if (!order.isPossibleToDelete()) {
       throw new CustomException(OrderErrorCode.UNABLE_REMOVE);
     }
-    order.delete(LocalDateTime.now(), 1L);
+
+    LocalDateTime now = LocalDateTime.now();
+    order.delete(now, userInfo.userId());
   }
 }
