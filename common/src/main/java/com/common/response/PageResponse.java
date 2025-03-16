9ecfde9f61b5f74354625f683c2cbe81806a3838
HanaHww2 <hanahww2@gmail.com>
2025-03-16 22:29:37 +0900

    [#53][common] feat: 페이지응답 레코드에 map() 메소드 추가 - dto 매핑용

--- common/src/main/java/com/common/response/PageResponse.java
+++ common/src/main/java/com/common/response/PageResponse.java
@@ -1,6 +1,7 @@
 package com.common.response;
 
 import java.util.List;
+import java.util.function.Function;
 import lombok.Builder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -24,5 +25,22 @@ public record PageResponse<T>(
         .pageNumber(pageable.getPageNumber() + 1)
         .pageSize(pageable.getPageSize())
         .build();
+  }
+
+  private static <U, T> PageResponse<U> from(PageResponse<T> PageResponse, List<U> list) {
+    return PageResponse.<U>builder()
+        .contents(list)
+        .totalElements(PageResponse.totalElements())
+        .totalPages(PageResponse.totalPages())
+        .pageNumber(PageResponse.pageNumber())
+        .pageSize(PageResponse.pageSize())
+        .build();
+  }
+
+  public <U> PageResponse<U> map(Function<T, U> mapper) {
+
+    return PageResponse.from(this, this.contents.<T>stream()
+        .map(t -> mapper.apply(t))
+        .toList());
   }
 }
