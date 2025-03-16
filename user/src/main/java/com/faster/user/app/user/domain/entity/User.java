d0759739bb2c05552f01ff2e88f29f52d72af54f
LimDae94 <piay801@naver.com>
2025-03-16 15:03:08 +0900

    [#29][user] feat: 회원 Domain 생성하기
    회원 등록 기준으로 필요한 정보를 생성하자.
    
    엔티티 생성 메서드 이름은 of로 했다. create 로 할지 고민했는데 검색해보니까 비즈니스 로직이 포함되면 create로 하고, 아니면 of로 하라고 한다. 비즈니스 로직이 포함되어 있지 않기 때문에 of를 그대로 사용했다.
    
    그리고 회원 가입하면 Role 은 null 로 설정하자는 의견에 따라서 일단은 회원 가입 직후에는 권한이 null 이다.
    
    각 필드에 @Column 타입과 길이는 전체적인 구현 이후 디테일하게 수정이 아마 필요할 것 같다.

---
+++ user/src/main/java/com/faster/user/app/user/domain/entity/User.java
@@ -0,0 +1,56 @@
+package com.faster.user.app.user.domain.entity;
+
+import com.common.domain.BaseEntity;
+import com.faster.user.app.user.domain.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_user")
+@Entity
+public class User extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(name = "username", length = 100, nullable = false, unique = true)
+  private String username;
+
+  @Column(name = "password", length = 500, nullable = false)
+  private String password;
+
+  @Column(name = "name", length = 100, nullable = false)
+  private String name;
+
+  @Column(name = "slack_id", length = 200, nullable = false)
+  private String slackId;
+
+  @Enumerated(EnumType.STRING)
+  private Role role;
+
+  private User(String username, String password, String name, String slackId) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.slackId = slackId;
+  }
+
+  public static User of(String username, String password, String name, String slackId) {
+    return new User(username, password, name, slackId);
+  }
+
+
+
+}
