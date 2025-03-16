5b74469ac277bd093cf7aea867374597f89b84fa
LimDae94 <piay801@naver.com>
2025-03-16 15:23:18 +0900

    [#29][auth] feactor: presentation 임포트 수정하기
    회원 생성은 auth에서 위임하기 때문에 auth로 이동했다. 이에 따라서 임포트 경로가 수정되었다.

--- user/src/main/java/com/faster/user/app/auth/presentation/dto/CreateUserResponseDto.java
+++ user/src/main/java/com/faster/user/app/auth/presentation/dto/CreateUserResponseDto.java
@@ -1,8 +1,17 @@
 package com.faster.user.app.auth.presentation.dto;
 
+import com.faster.user.app.user.domain.entity.User;
+
 public record CreateUserResponseDto(
     String username,
     String name,
     String slackId
 ) {
+  public static CreateUserResponseDto of(User savedUser) {
+    return new CreateUserResponseDto(
+        savedUser.getUsername(),
+        savedUser.getName(),
+        savedUser.getSlackId()
+    );
+  }
 }
