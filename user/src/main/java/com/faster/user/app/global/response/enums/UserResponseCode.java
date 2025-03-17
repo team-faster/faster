b703f50de86346c00c3ed73cb11c776d0a0d2364
LimDae94 <piay801@naver.com>
2025-03-17 11:06:15 +0900

    [#63][user] refactor: 회원 응답 수정하기
    회원의 응답 또한 ErrorCode 를 상속 받아서 동일한 포멧을 유지하자

--- user/src/main/java/com/faster/user/app/global/response/enums/UserResponseCode.java
+++ user/src/main/java/com/faster/user/app/global/response/enums/UserResponseCode.java
@@ -1,20 +1,17 @@
 package com.faster.user.app.global.response.enums;
 
+import com.common.exception.type.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserResponseCode {
+public enum UserResponseCode implements ErrorCode {
 
-  USER_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
-  USER_CREATED(HttpStatus.CREATED,  "회원가입이 완료되었습니다."),
-  USER_UPDATED(HttpStatus.OK,  "회원 정보가 수정되었습니다."),
-  USER_DELETED(HttpStatus.OK,  "회원이 삭제되었습니다."),
-  USER_FETCHED(HttpStatus.OK, "회원 정보를 성공적으로 조회했습니다."),
-  USERS_FETCHED(HttpStatus.OK, "전체 회원 목록을 성공적으로 조회했습니다.");
+  USER_UPDATED(HttpStatus.OK.value(), "회원 정보를 성공적으로 수정했습니다.", HttpStatus.OK);
 
-  private final HttpStatus status;
+  private final int code;
   private final String message;
+  private final HttpStatus status;
 }
