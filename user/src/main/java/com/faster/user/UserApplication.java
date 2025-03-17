6ef01a9bdace863ce018fa33a09984af6606b30f
LimDae94 <piay801@naver.com>
2025-03-17 17:56:52 +0900

    [#75][user] feat: 페이징 정보 형식 설정하기
    ```
            "page": {
                "size": 10,
                "number": 0,
                "totalElements": 2,
                "totalPages": 1
            }
    ```
    
    이렇게 나오도록 설정했다.

--- user/src/main/java/com/faster/user/UserApplication.java
+++ user/src/main/java/com/faster/user/UserApplication.java
@@ -2,7 +2,11 @@ package com.faster.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class UserApplication {
 
