c8aa3dfc149c228d54892ec9ffed72842b0e48ac
LimDae94 <piay801@naver.com>
2025-03-17 11:08:05 +0900

    [#63][user] refactor: 회원 응답 구현 및 컨벤션 따르기
    리포지토리 또한 persistence 안에서 구현체를 생성하자.
    회원 권한 설정을 구현한다.

--- user/src/main/java/com/faster/user/app/user/domain/repository/UserRepository.java
+++ user/src/main/java/com/faster/user/app/user/domain/repository/UserRepository.java
@@ -2,10 +2,8 @@ package com.faster.user.app.user.domain.repository;
 
 import com.faster.user.app.user.domain.entity.User;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository {
   Optional<User> findUserByUsername(String username);
   Optional<User> findUserByUsernameOrSlackId(String username, String slackId);
 }
---
+++ user/src/main/java/com/faster/user/app/user/presentation/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.faster.user.app.user.presentation.controller;
+
+import com.common.response.ApiResponse;
+import com.faster.user.app.global.response.enums.UserResponseCode;
+import com.faster.user.app.user.application.dto.UpdateUserRoleRequestDto;
+import com.faster.user.app.user.application.usecase.UserService;
+import com.faster.user.app.user.presentation.dto.UpdateUserRoleResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@RestController
+public class UserController {
+
+  private final UserService userService;
+
+  @PatchMapping("/{userId}/role")
+  public ResponseEntity<ApiResponse<UpdateUserRoleResponseDto>> updateUserRole(
+      @PathVariable Long userId,
+      @RequestBody UpdateUserRoleRequestDto requestDto) {
+    UpdateUserRoleResponseDto responseDto = userService.updateUserRoleByUserId(userId, requestDto);
+
+    return ResponseEntity
+        .status(UserResponseCode.USER_UPDATED.getStatus())
+        .body(new ApiResponse<>(
+            UserResponseCode.USER_UPDATED.getMessage(),
+            UserResponseCode.USER_UPDATED.getStatus().value(),
+            responseDto
+        ));
+  }
+}
