a44344c456425f4288b372b7e0df82b4b015491e
LimDae94 <piay801@naver.com>
2025-03-17 21:14:09 +0900

    [#75][user] 공통 응답으로 구현하기
    - page -> pageResponse
    - @AuthCheck 적용하기

--- user/src/main/java/com/faster/user/app/user/presentation/controller/UserController.java
+++ user/src/main/java/com/faster/user/app/user/presentation/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.faster.user.app.user.presentation.controller;
 
+import com.common.aop.annotation.AuthCheck;
+import com.common.resolver.dto.UserRole;
 import com.common.response.ApiResponse;
+import com.common.response.PageResponse;
 import com.faster.user.app.global.response.enums.UserResponseCode;
-import com.faster.user.app.user.application.dto.UpdateUserRoleRequestDto;
 import com.faster.user.app.user.application.usecase.UserService;
-import com.faster.user.app.user.presentation.dto.UpdateUserRoleResponseDto;
+import com.faster.user.app.user.infrastructure.persistence.jpa.dto.QUserProjection;
+import com.faster.user.app.user.presentation.dto.GetAllUserResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -21,4 +22,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+  private final UserService userService;
+
+  @AuthCheck(roles = {UserRole.ROLE_MASTER})
+  @GetMapping
+  public ResponseEntity<ApiResponse<PageResponse<GetAllUserResponseDto>>> getAllUsers(
+      @RequestParam(value = "username", defaultValue = "", required = false) String username,
+      @RequestParam(value = "name", defaultValue = "", required = false) String name,
+      @RequestParam(value = "slackId", defaultValue = "", required = false) String slackId,
+      @RequestParam(value = "page", defaultValue = "0") Integer page,
+      @RequestParam(value = "size", defaultValue = "10") Integer size) {
+    PageResponse<GetAllUserResponseDto> users = userService.getAllUsers(
+        username,
+        name,
+        slackId,
+        page,
+        size);
+
+    return ResponseEntity
+        .status(UserResponseCode.USER_UPDATED.getStatus())
+        .body(new ApiResponse<>(
+            UserResponseCode.USER_UPDATED.getMessage(),
+            UserResponseCode.USER_UPDATED.getStatus().value(),
+            users
+        ));
+  }
 }
