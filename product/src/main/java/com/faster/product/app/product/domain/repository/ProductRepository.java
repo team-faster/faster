4bae0269880e6c50fc43ca738281bb34728d18f0
HanaHww2 <hanahww2@gmail.com>
2025-03-18 02:52:11 +0900

    [#98][product] feat: 내부 상품 목록 조회

--- product/src/main/java/com/faster/product/app/product/domain/repository/ProductRepository.java
+++ product/src/main/java/com/faster/product/app/product/domain/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.faster.product.app.product.domain.repository;
 
 import com.faster.product.app.product.domain.entity.Product;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 public interface ProductRepository {
@@ -9,4 +11,6 @@ public interface ProductRepository {
   Optional<Product> findByIdAndDeletedAtIsNull(UUID productId);
 
   Product save(Product product);
+
+  List<Product> findByIdInAndDeletedAtIsNull(Set<UUID> ids);
 }
---
+++ product/src/main/java/com/faster/product/app/product/presentation/dto/response/GetProductsResponseDto.java
@@ -0,0 +1,53 @@
+package com.faster.product.app.product.presentation.dto.response;
+
+import com.faster.product.app.product.application.usecase.GetProductsApplicationResponseDto;
+import com.faster.product.app.product.application.usecase.GetProductsApplicationResponseDto.GetProductApplicationResponseDto;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import lombok.Builder;
+
+@Builder
+public record GetProductsResponseDto(
+    Map<UUID, GetProductResponseDto> products
+) {
+
+  public static GetProductsResponseDto from(GetProductsApplicationResponseDto applicationResponseDto) {
+    return GetProductsResponseDto.builder()
+        .products(applicationResponseDto.products()
+            .stream()
+            .collect(Collectors.toMap(
+                GetProductApplicationResponseDto::id,
+                GetProductResponseDto::from
+            )))
+        .build();
+
+  }
+
+  @Builder
+  record GetProductResponseDto(
+      UUID id,
+      UUID hubId,
+      UUID companyId,
+      String companyName,
+      String name,
+      BigDecimal price,
+      Integer quantity,
+      String description
+  ) {
+
+    public static GetProductResponseDto from(GetProductApplicationResponseDto applicationResponseDto) {
+      return GetProductResponseDto.builder()
+          .id(applicationResponseDto.id())
+          .hubId(applicationResponseDto.hubId())
+          .companyId(applicationResponseDto.companyId())
+          .companyName(applicationResponseDto.companyName())
+          .name(applicationResponseDto.name())
+          .price(applicationResponseDto.price())
+          .quantity(applicationResponseDto.quantity())
+          .description(applicationResponseDto.description())
+          .build();
+    }
+  }
+}
