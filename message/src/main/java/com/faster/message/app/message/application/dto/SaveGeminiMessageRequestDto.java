4ad9b866deda802ccec49550f2328d3516bbe6f8
LimDae94 <piay801@naver.com>
2025-03-17 22:50:34 +0900

    [#90][message]feat: 메시지 타입 추가

--- message/src/main/java/com/faster/message/app/message/application/dto/SaveGeminiMessageRequestDto.java
+++ message/src/main/java/com/faster/message/app/message/application/dto/SaveGeminiMessageRequestDto.java
@@ -14,7 +14,7 @@ public record SaveGeminiMessageRequestDto(
     String source, // 출발지
     String waypoint, // 경유지
     String destination, // 목적지
-    MessageType type, // 메시지 타입
+    MessageType messageType, // 메시지 타입
     LocalDateTime sendAt, // 메시지 발송 일자
     String deliveryManager, // 배송 담당자
     String deliveryManagerSlackId // 배송 담당자 슬랙 ID
