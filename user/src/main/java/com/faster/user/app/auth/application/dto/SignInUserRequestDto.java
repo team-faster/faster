b76547225e3069198d3ae29a47fffd8d2d279337
LimDae94 <piay801@naver.com>
2025-03-17 11:01:58 +0900

    [#63][auth] feat: 애플리케이션 회원 로그인 기능 구현하기
    컨벤션에 따라서 이름 글자를 84 -> 100 수정하자.
    컨벤션에 따라 usecase -> impl 로 수정하자.
    회원 로그인에 성공하면 액세스 토큰과 리프레시 토큰을 반환하자.
    
    -> 레디스 안에 리프레시 토큰 저장이 필요!

--- user/src/main/java/com/faster/user/app/auth/application/dto/SignInUserRequestDto.java
+++ user/src/main/java/com/faster/user/app/auth/application/dto/SignInUserRequestDto.java
@@ -1,11 +1,9 @@
 package com.faster.user.app.auth.application.dto;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record CreateUserRequestDto(
-
+public record SignInUserRequestDto(
     @NotBlank(message = "아이디: 필수 정보입니다.")
     @Pattern(
         regexp = "^[a-z0-9]{4,10}$",
@@ -18,21 +16,6 @@ public record CreateUserRequestDto(
         regexp = "^[A-Za-z\\d@$!%*?&]{8,15}$",
         message = "비밀번호: 최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자를 사용할 수 있습니다."
     )
-    String password,
-
-    @NotBlank(message = "이름: 필수 정보입니다.")
-    @Pattern(
-        regexp = "^[가-힣a-zA-Z]{1,84}$",
-        message = "이름: 최소 1자 이상, 100자 이하이며 한글 또는 영문자만 사용할 수 있습니다."
-    )
-    String name,
-
-    @NotBlank(message = "이메일: 필수 정보입니다.")
-    @Email(message = "이메일: 이메일 형식이 올바르지 않습니다")
-    @Pattern(
-        regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
-        message = "이메일: 이메일 형식이 올바르지 않습니다."
-    )
-    String slackId
+    String password
 ) {
 }
