6ce64026a6a078d02060965fa6091823f4d7fc2f
HanaHww2 <hanahww2@gmail.com>
2025-03-18 15:55:04 +0900

    [#113][product] feat: feignClient 관련 설정 추가

--- product/src/main/java/com/faster/product/app/global/config/FeignClientConfig.java
+++ product/src/main/java/com/faster/product/app/global/config/FeignClientConfig.java
@@ -1,11 +1,33 @@
 package com.faster.product.app.global.config;
 
+import feign.RequestInterceptor;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableFeignClients("com.faster.product.app.product.infrastructure.feign")
 public class FeignClientConfig {
+  @Bean
+  public RequestInterceptor requestInterceptor() {
+
+    return requestTemplate -> {
+      ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+      HttpServletRequest request = attributes.getRequest();
+
+      String userIdStr = request.getHeader("X-User-Id");
+      String userRoleStr = request.getHeader("X-User-Role");
+      if (userIdStr != null) {
+        requestTemplate.header("X-User-Id", userIdStr);
+      }
+      if (userRoleStr != null) {
+        requestTemplate.header("X-User-Role", userRoleStr);
+      }
+    };
+  }
 }
