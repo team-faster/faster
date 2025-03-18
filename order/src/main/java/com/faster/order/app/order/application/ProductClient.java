280a45cab47e46944bf92ae691bfea0ba871acd5
HanaHww2 <hanahww2@gmail.com>
2025-03-18 15:45:39 +0900

    [#105][order] feat: productClient 기능 구현

--- order/src/main/java/com/faster/order/app/order/application/ProductClient.java
+++ order/src/main/java/com/faster/order/app/order/application/ProductClient.java
@@ -1,5 +1,14 @@
 package com.faster.order.app.order.application;
 
+import com.faster.order.app.order.application.dto.request.GetProductsApplicationResponseDto;
+import com.faster.order.app.order.application.dto.request.UpdateStocksApplicationRequestDto;
+import com.faster.order.app.order.application.dto.response.UpdateStocksApplicationResponseDto;
+import java.util.Set;
+import java.util.UUID;
+
 public interface ProductClient {
 
+  GetProductsApplicationResponseDto getProducts(Set<UUID> ids);
+
+  UpdateStocksApplicationResponseDto updateStocks(UpdateStocksApplicationRequestDto requests);
 }
--- order/src/main/java/com/faster/order/app/order/infrastructure/ProductClientImpl.java
+++ order/src/main/java/com/faster/order/app/order/infrastructure/ProductClientImpl.java
@@ -1,7 +1,16 @@
 package com.faster.order.app.order.infrastructure;
 
 import com.faster.order.app.order.application.ProductClient;
+import com.faster.order.app.order.application.dto.request.GetProductsApplicationResponseDto;
+import com.faster.order.app.order.application.dto.request.UpdateStocksApplicationRequestDto;
+import com.faster.order.app.order.application.dto.response.UpdateStocksApplicationResponseDto;
 import com.faster.order.app.order.infrastructure.feign.ProductFeignClient;
+import com.faster.order.app.order.infrastructure.feign.dto.request.UpdateStocksRequestDto;
+import com.faster.order.app.order.infrastructure.feign.dto.response.GetProductsResponseDto;
+import com.faster.order.app.order.infrastructure.feign.dto.response.UpdateStocksResponseDto;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,4 +18,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ProductClientImpl implements ProductClient {
   private final ProductFeignClient productFeignClient;
+
+  @Override
+  public GetProductsApplicationResponseDto getProducts(Set<UUID> ids) {
+    GetProductsResponseDto responseDto = productFeignClient.getProducts(ids).getBody().data();
+    return responseDto.toApplicationDto();
+  }
+
+  @Override
+  public UpdateStocksApplicationResponseDto updateStocks(UpdateStocksApplicationRequestDto requests) {
+    UpdateStocksResponseDto responseDto = productFeignClient.updateStocks(
+        UpdateStocksRequestDto.from(requests)).getBody().data();
+    return responseDto.toApplicationDto();
+  }
 }
