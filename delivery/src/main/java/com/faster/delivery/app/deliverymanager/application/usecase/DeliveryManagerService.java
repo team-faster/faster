081d1598e77f5ae16f2f97080844d996383feded
je-pa <jieun0502@gmail.com>
2025-03-21 03:49:49 +0900

    [#178][deliverymanager] feat: 업체 배송 담당자 배정 내부 요청 api 구현

--- delivery/src/main/java/com/faster/delivery/app/deliverymanager/application/usecase/DeliveryManagerService.java
+++ delivery/src/main/java/com/faster/delivery/app/deliverymanager/application/usecase/DeliveryManagerService.java
@@ -1,9 +1,11 @@
 package com.faster.delivery.app.deliverymanager.application.usecase;
 
 import com.common.resolver.dto.CurrentUserInfoDto;
+import com.faster.delivery.app.deliverymanager.application.dto.AssignCompanyDeliveryManagerApplicationResponse;
 import com.faster.delivery.app.deliverymanager.application.dto.DeliveryManagerDetailDto;
 import com.faster.delivery.app.deliverymanager.application.dto.DeliveryManagerSaveDto;
 import com.faster.delivery.app.deliverymanager.application.dto.DeliveryManagerUpdateDto;
+import com.faster.delivery.app.deliverymanager.presentation.dto.internal.AssignCompanyDeliveryManagerResponse;
 import java.util.UUID;
 
 public interface DeliveryManagerService {
@@ -13,4 +15,6 @@ public interface DeliveryManagerService {
   UUID deleteDeliveryManager(UUID deliveryManagerId, CurrentUserInfoDto userInfo);
 
   DeliveryManagerDetailDto getDeliveryManagerDetailInternal(CurrentUserInfoDto userInfo, UUID deliveryManagerId);
+
+  AssignCompanyDeliveryManagerApplicationResponse assignCompanyDeliveryManger(UUID hubId);
 }
