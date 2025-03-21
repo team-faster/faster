bcb2b3aa366a678d4745cb0b85a82a8790bfebca
LimDae94 <piay801@naver.com>
2025-03-21 20:31:00 +0900

    [message][#194] feat: 슬랙 생성과 열기에서 실패 응답 구현
    슬랙 공식 문서에서 DM이 열려있지 않으면 열어줘야 한다고 하기 때문에, 열어 줄 때를 고려해서 실패 응답도 함께 생성 했다.

--- message/src/main/java/com/faster/message/app/global/enums/MessageErrorCode.java
+++ message/src/main/java/com/faster/message/app/global/enums/MessageErrorCode.java
@@ -13,7 +13,12 @@ public enum MessageErrorCode implements ErrorCode {
   MESSAGE_INVALID_SEND_AT(HttpStatus.BAD_REQUEST.value(), "현재 시간보다 이전으로 메시지를 보낼 수 없습니다.", HttpStatus.BAD_REQUEST),
   MESSAGE_INVALID_SLACK_ID(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 Slack ID 입니다.", HttpStatus.BAD_REQUEST),
   MESSAGE_TYPE_NULL(HttpStatus.BAD_REQUEST.value(), "메시지 타입이 비어 있습니다.", HttpStatus.BAD_REQUEST),
-  MESSAGE_TYPE_INVALID(HttpStatus.BAD_REQUEST.value(), "메시지 타입이 유효하지 않습니다.", HttpStatus.BAD_REQUEST);
+  MESSAGE_TYPE_INVALID(HttpStatus.BAD_REQUEST.value(), "메시지 타입이 유효하지 않습니다.", HttpStatus.BAD_REQUEST),
+
+  // Slack
+  SLACK_OPEN_DM_FAILED(HttpStatus.BAD_REQUEST.value(), "DM 채널을 열 수 없습니다.", HttpStatus.BAD_REQUEST),
+  SLACK_CREATE_DM_FAILED(HttpStatus.BAD_REQUEST.value(), "DM 채널을 생성할 수 없습니다.", HttpStatus.BAD_REQUEST);
+
 
   private final int code;
   private final String message;
