fb9755a72ccfb7db55b628a405ae5746c4276712
LimDae94 <piay801@naver.com>
2025-03-22 12:42:04 +0900

    [#206][message] feat: 메시지 엔티티 수정
    올바르게 다시 수정한다.

--- message/src/main/java/com/faster/message/app/message/domain/repository/MessageRepository.java
+++ message/src/main/java/com/faster/message/app/message/domain/repository/MessageRepository.java
@@ -1,8 +1,14 @@
 package com.faster.message.app.message.domain.repository;
 
 import com.faster.message.app.message.domain.entity.Message;
+import com.faster.message.app.message.presentation.dto.response.PGetAllMessageResponseDto;
+import java.time.LocalDate;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MessageRepository {
   Message save(Message message);
+
+  Page<PGetAllMessageResponseDto> searchMessages(UUID targetSlackId, String content, String messageType, LocalDate sendAt, int page, int size);
 }
