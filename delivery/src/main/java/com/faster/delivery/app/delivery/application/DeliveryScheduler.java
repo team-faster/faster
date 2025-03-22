8dca9f98b71607340983204000bf85c729714019
je-pa <jieun0502@gmail.com>
2025-03-22 12:03:25 +0900

    [#190][deliverymanager] feat: serivce 호출 스케줄러 및 컨트롤러

---
+++ delivery/src/main/java/com/faster/delivery/app/delivery/application/DeliveryScheduler.java
@@ -0,0 +1,27 @@
+package com.faster.delivery.app.delivery.application;
+
+import com.faster.delivery.app.delivery.application.dto.AssignedDeliveryRouteDto;
+import com.faster.delivery.app.delivery.application.usecase.DeliveryService;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class DeliveryScheduler {
+
+  private final DeliveryService deliveryService;
+
+  public DeliveryScheduler(DeliveryService deliveryService) {
+    this.deliveryService = deliveryService;
+  }
+
+  @Scheduled(cron = "0 0 10,16 * * ?") // 매일 오전 10시, 오후 4시 실행
+  public void assignHubDelivery() {
+    log.info("Scheduled task executed at: {}", LocalDateTime.now());
+    List<AssignedDeliveryRouteDto> assignedRoutes = deliveryService.assignHubDeliveryManagerScheduleService();
+    log.info("Assigned Hub Delivery Routes : {}", assignedRoutes.size());
+  }
+}
---
+++ delivery/src/main/java/com/faster/delivery/app/global/config/EnableSchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.faster.delivery.app.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class EnableSchedulingConfig {
+
+}
